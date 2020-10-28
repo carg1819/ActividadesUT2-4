f8ddb0428f00e2be2c5ea2ea93200913ad5b99a3
Carlos Riaño <ca.riano@iesdoctorbalmis.com>
2020-10-28 11:29:24 +0100

    Agregar archivos de proyecto.

---
+++ ActividadesUT2-4/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ActividadesUT2_4
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtbEscrito_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtLimite.Content = txtbEscrito.Text.Length + "/140";
+            if(txtbEscrito.Text.Length == 140)
+            {
+                txtbEscrito.IsReadOnly = true;
+            }
+        }
+    }
+}
